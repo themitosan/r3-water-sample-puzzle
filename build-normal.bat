0225ed31a0dbf77402caa85fa65553abfc9008c7
themitosan <temmieheartz@yahoo.com>
2023-07-07 18:14:31 -0300

    Build: Include manual files

---
+++ build-normal.bat
@@ -0,0 +1,24 @@
+::===================================::
+::									 ::
+::  R3 Water Sample Puzzle			 ::
+::  build-normal.bat				 ::
+::									 ::
+::  Build file for windows 		  	 ::
+::									 ::
+::===================================::
+@echo off
+cls
+title Running compiler - Please wait... 
+echo.
+echo Running compiler - Please wait...
+echo Flavor: normal
+echo.
+echo IMPORTANT: Make sure to have Node.js (20.x) installed before running this script!
+echo You can get it here: https://nodejs.org/
+echo.
+node ./compiler/comp-prepFiles.js --mainJs=app.js --copyFolders=img,tools,forms,node_modules --exportJsSample=!1 --cssMinifyLevel=2 --jsBeforeMain=systemjs.js,named-register.min.js
+node ./compiler/comp-nw.js --platform=win64 --flavor=normal --nwVersion=0.77.0
+title Process complete!
+echo.
+pause
+exit
